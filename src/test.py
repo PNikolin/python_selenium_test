a705d3909f660f3903547d355f15c121ce1dee51
pavel-nikolin <nikolin.pasha@gmail.com>
2019-10-16 18:55:49 +0300

    updated uri

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome("D:\Downloads\Python\chromedriver.exe")
-driver.get("https://wikipedia.org")
+driver.get("https://www.wikipedia.org/")
 
 search_field = driver.find_element_by_id("searchInput")
 search_button = driver.find_element_by_xpath("//form[@id = 'search-form']/fieldset/button")
@@ -9,7 +9,7 @@ search_button = driver.find_element_by_xpath("//form[@id = 'search-form']/fields
 search_field.send_keys("Test Automation")
 search_button.click()
 
-assert "Test automation - Wikipedia" in driver.title
+assert "Test Automation" in driver.title
 driver.quit()
 
 
